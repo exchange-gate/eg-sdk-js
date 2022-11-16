b32af60cf28e634a2d686ac877dd6f9ce5c5fdb6
Daniels Leiba <leibadaniels@gmail.com>
2022-11-16 15:39:40 +0200

    test fixes

--- src/Tests/exchanger.test.ts
+++ src/Tests/exchanger.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-restricted-imports */
 import { ExchangeGate } from '@exchange-gate.io/eg-sdk-js';
 import {
     LimitOrderParams,
@@ -7,8 +6,9 @@ import {
     ResponseState,
     TickerType
 } from '@exchange-gate.io/eg-sdk-js/types/src/Types/response';
+import { Exchanger } from '@Types/response';
 import jwtDecode from 'jwt-decode';
-
+import { expect, jest, test, describe } from '@jest/globals';
 
 // eslint-disable-next-line max-len
 let API_KEY = '';
@@ -17,7 +17,6 @@ const config = {
     baseURL: 'https://staging-api.exchange-gate.io'
 };
 const egRest = new ExchangeGate.Rest(API_KEY, config);
-
 const markets = [
     'btc-usdt',
     'btc-eth'
@@ -27,91 +26,115 @@ const exchangerKey = ''; // must fill
 const exchangerSecret = ''; // must fill
 const periodFrom = '';
 const periodTo = '';
-let exchanger = '';
+const exchanger = 'binance';
 let exchangerKeyId = 0;
 let market = '';
 let uuid = '';
 let generalKey = '';
 let generalKeyId = 0;
+let exchangersArray: Exchanger[] = [];
 
-test('fetchExchangers', async () => {
-    const exchangers = await egRest.fetchExchangers();
-    if (exchangers.data) {
-        exchanger = exchangers.data[0].name;
-    }
-    expect(exchangers.state).toBe(ResponseState.SUCCESS);
-    expect(exchangers.data?.length).toBeGreaterThanOrEqual(1);
+jest.setTimeout(10000);
+
+describe('Fetch Exchanger Data', () => {
+    test('fetchExchangers', async () => {
+        const exchangers = await egRest.fetchExchangers();
+        if (exchangers.data) {
+            exchangersArray = exchangers.data;
+        }
+        expect(exchangers.state).toBe(ResponseState.SUCCESS);
+        expect(exchangers.data?.length).toBeGreaterThanOrEqual(1);
+    });
+
+    test('fetchMarkets', async () => {
+        const markets = await egRest.fetchMarkets();
+        if (markets.data) {
+            market = markets.data[0].market;
+        }
+        expect(markets.state).toBe(ResponseState.SUCCESS);
+        expect(markets.data?.length).toBeGreaterThanOrEqual(1);
+    });
+
+    test('fetchExchangerMarkets', async () => {
+        const markets = await egRest.fetchExchangerMarkets();
+        expect(markets.state).toBe(ResponseState.SUCCESS);
+        expect(markets.data).not.toBeUndefined();
+    });
 });
 
-test('fetchMarkets', async () => {
-    const markets = await egRest.fetchMarkets();
-    if (markets.data) {
-        market = markets.data[0].market;
-    }
-    expect(markets.state).toBe(ResponseState.SUCCESS);
-    expect(markets.data?.length).toBeGreaterThanOrEqual(1);
+describe.each(exchangersArray)('fetchOrderBookSnapshot', (exchanger: Exchanger) => {
+    test(`fetchOrderBookSnapshot -  ${exchanger.name}`, async () => {
+        const orderBook = await egRest.fetchOrderBookSnapshot(exchanger.name, market);
+        expect(orderBook.state).toBe(ResponseState.SUCCESS);
+        expect(orderBook.data).not.toBeUndefined();
+    });
 });
 
-test('fetchExchangerMarkets', async () => {
-    const markets = await egRest.fetchExchangerMarkets();
-    expect(markets.state).toBe(ResponseState.SUCCESS);
-    expect(markets.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchPublicTradesSnapshot', (exchanger: Exchanger) => {
+    test(`fetchPublicTradesSnapshot -  ${exchanger.name}`, async () => {
+        const publicTrades = await egRest.fetchPublicTradesSnapshot(exchanger.name, market);
+        expect(publicTrades.state).toBe(ResponseState.SUCCESS);
+        expect(publicTrades.data).not.toBeUndefined();
+    });
 });
 
-test('fetchOrderBookSnapshot', async () => {
-    const orderBook = await egRest.fetchOrderBookSnapshot(exchanger, market);
-    expect(orderBook.state).toBe(ResponseState.SUCCESS);
-    expect(orderBook.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchOhlcv', (exchanger: Exchanger) => {
+    test(`fetchOhlcv -  ${exchanger.name}`, async () => {
+        const group = '1MTH';
+        const publicTrades = await egRest.fetchOhlcv(exchanger.name, market, group, periodFrom, periodTo);
+        expect(publicTrades.state).toBe(ResponseState.SUCCESS);
+        expect(publicTrades.data).not.toBeUndefined();
+    });
 });
 
-test('fetchPublicTradesSnapshot', async () => {
-    const publicTrades = await egRest.fetchPublicTradesSnapshot(exchanger, market);
-    expect(publicTrades.state).toBe(ResponseState.SUCCESS);
-    expect(publicTrades.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchHistoricalTrades', (exchanger: Exchanger) => {
+    test(`fetchHistoricalTrades -  ${exchanger.name}`, async () => {
+        const historicalTrades = await egRest.fetchHistoricalTrades(exchanger.name, market, periodFrom, periodTo);
+        expect(historicalTrades.state).toBe(ResponseState.SUCCESS);
+        expect(historicalTrades.data).not.toBeUndefined();
+    });
 });
 
-test('fetchOhlcv', async () => {
-    const group = '1MTH';
-    const publicTrades = await egRest.fetchOhlcv(exchanger, market, group, periodFrom, periodTo);
-    expect(publicTrades.state).toBe(ResponseState.SUCCESS);
-    expect(publicTrades.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchMyTrades', (exchanger: Exchanger) => {
+    test(`fetchMyTrades -  ${exchanger.name}`, async () => {
+        const myTrades = await egRest.fetchMyTrades(exchanger.name, market);
+        expect(myTrades.state).toBe(ResponseState.SUCCESS);
+        expect(myTrades.data).not.toBeUndefined();
+    });
 });
 
-test('fetchHistoricalTrades', async () => {
-    const historicalTrades = await egRest.fetchHistoricalTrades(exchanger, market, periodFrom, periodTo);
-    expect(historicalTrades.state).toBe(ResponseState.SUCCESS);
-    expect(historicalTrades.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchOpenOrders', (exchanger: Exchanger) => {
+    test(`fetchOpenOrders -  ${exchanger.name}`, async () => {
+        const openOrders = await egRest.fetchOpenOrders(exchanger.name, market);
+        expect(openOrders.state).toBe(ResponseState.SUCCESS);
+        expect(openOrders.data).not.toBeUndefined();
+    });
 });
 
-test('fetchMyTrades', async () => {
-    const myTrades = await egRest.fetchMyTrades(exchanger, market);
-    expect(myTrades.state).toBe(ResponseState.SUCCESS);
-    expect(myTrades.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchTicker', (exchanger: Exchanger) => {
+    test(`fetchTicker -  ${exchanger.name}`, async () => {
+        const ticker = await egRest.fetchTicker(exchanger.name, markets, TickerType.PRICE);
+        expect(ticker.state).toBe(ResponseState.SUCCESS);
+        expect(ticker.data).not.toBeUndefined();
+    });
 });
 
-test('fetchOpenOrders', async () => {
-    const openOrders = await egRest.fetchOpenOrders(exchanger, market);
-    expect(openOrders.state).toBe(ResponseState.SUCCESS);
-    expect(openOrders.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchPriceTicker', (exchanger: Exchanger) => {
+    test(`fetchPriceTicker -  ${exchanger.name}`, async () => {
+        const priceTicker = await egRest.fetchPriceTicker(exchanger.name, markets);
+        expect(priceTicker.state).toBe(ResponseState.SUCCESS);
+        expect(priceTicker.data).not.toBeUndefined();
+    });
 });
 
-test('fetchTicker', async () => {
-    const ticker = await egRest.fetchTicker(exchanger, markets, TickerType.PRICE);
-    expect(ticker.state).toBe(ResponseState.SUCCESS);
-    expect(ticker.data).not.toBeUndefined();
+describe.each(exchangersArray)('fetchOrderBookTicker', (exchanger: Exchanger) => {
+    test(`fetchOrderBookTicker -  ${exchanger.name}`, async () => {
+        const orderBookTicker = await egRest.fetchOrderBookTicker(exchanger.name, markets);
+        expect(orderBookTicker.state).toBe(ResponseState.SUCCESS);
+        expect(orderBookTicker.data).not.toBeUndefined();
+    });
 });
 
-test('fetchPriceTicker', async () => {
-    const priceTicker = await egRest.fetchPriceTicker(exchanger, markets);
-    expect(priceTicker.state).toBe(ResponseState.SUCCESS);
-    expect(priceTicker.data).not.toBeUndefined();
-});
-
-test('fetchOrderBookTicker', async () => {
-    const orderBookTicker = await egRest.fetchOrderBookTicker(exchanger, markets);
-    expect(orderBookTicker.state).toBe(ResponseState.SUCCESS);
-    expect(orderBookTicker.data).not.toBeUndefined();
-});
 
 test('createOrder', async () => {
     const orderParams: OrderParams = {
